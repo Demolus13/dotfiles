f03fcd1111ffc494bfb9b54213760e62ed3e0fa9
Demolus13 <parthgovale@gmail.com>
2025-02-27 17:52:50 +0530

    Updated configurations

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,23 +1,48 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  event = "BufReadPost",
-  opts = {
-    suggestion = {
-      enabled = false,
-      auto_trigger = true,
-      hide_during_completion = vim.g.ai_cmp,
-      keymap = {
-        accept = false, -- handled by nvim-cmp / blink.cmp
-        next = "<A-]>",
-        prev = "<A-[>",
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    opts = {
+      suggestion = {
+        enabled = false,
+        auto_trigger = true,
+        hide_during_completion = vim.g.ai_cmp,
+        keymap = {
+          accept = false, -- handled by nvim-cmp / blink.cmp
+          next = "<A-]>",
+          prev = "<A-[>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
       },
     },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-      help = true,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      model = "DeepSeek-R1", -- Set the default model to deepseek
+    },
+    keys = {
+      { "<C-c>c", ":CopilotChatToggle<CR>", mode="n", desc="Chat with Copilot" },
+      { "<C-c>m", ":CopilotChatModels<CR>", mode="n", desc="Select Copliot Model" },
+      { "<C-c>a", ":CopilotChatAgents<CR>", mode="n", desc="Select Copilot Agent" },
+      { "<C-c>e", ":CopilotChatExplain<CR>", mode="v", desc="Explain Copilot" },
+      { "<C-c>r", ":CopilotChatReview<CR>", mode="v", desc="Review Code" },
+      { "<C-c>f", ":CopilotChatFix<CR>", mode="v", desc="Fix Code Issues" },
+      { "<C-c>o", ":CopilotChatOptimize<CR>", mode="v", desc="Optimize Code" },
+      { "<C-c>d", ":CopilotChatDocs<CR>", mode="v", desc="Generate Docs" },
+      { "<C-c>t", ":CopilotChatTests<CR>", mode="v", desc="Generate Tests" },
+    }
   },
 }
+
